15e6d9e9871f7bfd82a39ca9ef680b5391bcd0cb
Christopher Klapp <christopher@klapp.name>
2018-10-16 17:10:57 -0400

    fs cleanup

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+import './Application.css';
+
 import ApplicationBar from './ApplicationBar';
 import Workspace from './Workspace';
 
